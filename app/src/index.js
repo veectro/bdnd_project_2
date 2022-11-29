2fe8cf53f03fbe68588796063a39a1928adaf77f
veectro <veectro@gmail.com>
2022-11-30 00:16:32 +0100

    chore: add implementation of index.js

--- app/src/index.js
+++ app/src/index.js
@@ -41,7 +41,10 @@ const App = {
 
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
-    
+    const lookid = document.getElementById("lookid").value;
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    let starName = await lookUptokenIdToStarInfo(lookid).call();
+    App.setStatus("Star name is " + starName + ".");
   }
 
 };

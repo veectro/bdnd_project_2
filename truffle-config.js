8c501876db27215e942ee9b19728e241611b06f4
veectro <veectro@gmail.com>
2022-11-30 01:28:25 +0100

    chore: update hdwallet provider

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,12 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+// https://ethereum.stackexchange.com/questions/62574/error-cannot-find-module-truffle-hdwallet-provider
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -52,6 +53,10 @@ module.exports = {
       network_id: "*",       // Any network (default: none)
     },
 
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+      network_id: 2111   // This network is yours, in the cloud.
+    }
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
